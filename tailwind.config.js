ef2cbf76ab335b924d4f2c38beb08d929eec61df
[Moeez Rashid] <[moeezrashidd@gmail.com]>
2025-08-05 23:21:25 +0500

    fixing skills section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
-
-
 // tailwind.config.js
 export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}", // important!
+  "./src/**/*.{js,jsx,ts,tsx}", // Make sure all your component paths are included
 ];
+
+export const safelist = [
+  'text-[#E34C26]',
+  'text-[#2565ae]',
+  'text-[#f0db4f]',
+  'text-[#06B6D4]',
+  'text-[#61DBFB]',
+  'text-[#ffd43b]',
+  'text-[#00758f]',
+  'text-[#f29111]',
+  'text-[#2d6f56]',
+  'text-[#fc6d27]',
+  'text-[#666666]',
+  'text-[#764ABC]',
+  'text-[#EC5990]',
+  'text-[#2196F3]',
+  'text-[#03599C]',
+];
+
 export const theme = {
   extend: {},
 };
+
 export const plugins = [];
